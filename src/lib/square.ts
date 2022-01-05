605f1cd9da3c253074eee2d8a46b7189be8f6d5f
griffinodow <griffin@griffindow.com>
2022-01-05 11:53:22 -0800

    Add comments to square lib

--- src/lib/square.ts
+++ src/lib/square.ts
@@ -1,5 +1,8 @@
 import { Client, Environment } from 'square'
 
+/**
+ * Initializes the square client.
+ */
 export const square = new Client({
   environment: Environment.Sandbox,
   accessToken: process.env.GS_SQUARE_ACCESS_TOKEN
