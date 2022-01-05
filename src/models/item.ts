5a666f52e4431145b5a7117f48867617fad64328
griffinodow <griffin@griffindow.com>
2022-01-05 14:30:53 -0800

    Add comment and refactor item

--- src/models/item.ts
+++ src/models/item.ts
@@ -1,5 +1,8 @@
 import { ItemVariation } from './item_variation'
 
+/**
+ * An item in the Square catalog.
+ */
 export class Item {
   id: string
   categoryId: string
@@ -8,7 +11,12 @@ export class Item {
   url: string
   variations: ItemVariation[] = []
 
-  constructor (id: string, categoryId: string, name: string, description: string, url: string) {
+  constructor (
+    id: string,
+    categoryId: string,
+    name: string,
+    description: string,
+    url: string) {
     this.id = id
     this.categoryId = categoryId
     this.name = name
